be1433e04192b6b875f70ea1521f24390e02754b
Muj <muji2498@users.noreply.github.com>
2025-01-23 08:38:32 +0000

    Change version

--- Plugin.cs
+++ Plugin.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace CommandMod;
 
-[BepInPlugin("me.muj.commandmod", "CommandMod","1.0.4")]
+[BepInPlugin("me.muj.commandmod", "CommandMod","1.0.5")]
 public class Plugin : BaseUnityPlugin
 {
     public static Plugin Instance { get; private set; } = null!;
